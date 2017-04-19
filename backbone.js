e91cb912c1c627048306f846dafb37fc06cf6eb8
Rodrigo Buas <rodrigobuas@gmail.com>
2017-04-19 12:07:41 +0200

    correction in quotemodel require at the backbone

--- backbone.js
+++ backbone.js
@@ -1,7 +1,7 @@
 module.exports.MemoRouter = require("memodb").MemoRouter;
 module.exports.MemoModel = require("memodb").MemoModel;
 module.exports.WapModel = require("memodb").WapModel;
-module.exports.QuoteModel = require("quote").QuoteModel;
+module.exports.QuoteModel = require("memodb").QuoteModel;
 
 module.exports.Brain = require("./brain");
 module.exports.Heart = require("./heart");
